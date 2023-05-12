b45a148e253b06a9550e6ab18bcf32b014c1fbab
Erin Timlin <erintimlin@gmail.com>
2023-05-12 18:34:30 -0400

    add main comic list, add image

--- src/components/ComicList.js
+++ src/components/ComicList.js
@@ -1,21 +1,40 @@
 import React from "react";
 import Comic from "./Comic";
+import Comic1 from "./../img/Comic1.jpg";
+import Comic2 from "./../img/Comic2.jpg";
+
+const mainComicList = [
+  {
+    title: "this is a comic",
+    condition: "F-",
+    date: "some date",
+    price: "$10",
+    image: Comic1
+  },
+  {
+    title: "this is a comic also",
+    condition: "F",
+    date: "some other date",
+    price: "$40",
+    image: Comic2
+  }
+];
 
 
 function ComicList() {
   return (
     <React.Fragment>
-    <Comic 
-    // <img src={Comic1} alt="Fake Comic" />
-    title="this is a comic"
-    condition="F-"
-    date="some date"
-    price="$10"/>
-    <Comic
-    title="this is a comic also"
-    condition="F"
-    date="some other date"
-    price="$40"/>
+      {mainComicList.map((comic, index) =>
+        <Comic 
+        title={comic.title}
+        condition={comic.condition}
+        date={comic.date}
+        price={comic.price}
+        image={comic.image}
+        key={index}
+        />
+      )}
+
     </React.Fragment>
   );
 }
