f9feeff77a0d91647c21216578c5ef06a68f9e13
Erin Timlin <erintimlin@gmail.com>
2023-05-12 18:33:37 -0400

    fix import, add img to prop, fix style for img

--- src/components/Comic.js
+++ src/components/Comic.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Comic1 from "./../img/Comic1.jpg";
-// import PropTypes from "prop-types";
+// import Comic1 from "./../img/Comic1.jpg";
+import PropTypes from "prop-types";
 
 function Comic(props){
+  const imageStyle = {
+    width: "200px",
+    height: "300px",
+  };
+
+
   return (
     <React.Fragment>
-      <img src={Comic1} alt="Fake Comic" />
+      {/* <img src={Comic1} alt="Fake Comic" /> */}
+      <img src={props.image} alt="Comic" style={imageStyle} />
       <h3>{props.title}</h3>
       <h4>Condition: {props.condition}</h4>
       <h4>Date: {props.date}</h4>
@@ -14,11 +21,12 @@ function Comic(props){
   );
 }
 
-// Comic.propTypes = {
-//   title: PropTypes.string,
-//   condition: PropTypes.string,
-//   date: PropTypes.string,
-//   price: PropTypes.string,
-// }
+Comic.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string,
+  condition: PropTypes.string,
+  date: PropTypes.string,
+  price: PropTypes.string,
+}
 
 export default Comic;
