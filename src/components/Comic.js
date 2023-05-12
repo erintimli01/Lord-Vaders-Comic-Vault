856df5da4079202393f9c07c37d06c3f24d82d18
Erin Timlin <erintimlin@gmail.com>
2023-05-12 16:27:26 -0400

    add props

--- src/components/Comic.js
+++ src/components/Comic.js
@@ -1,14 +1,24 @@
 import React from "react";
-import Comic1 from "./../img/Comic1.jpg"
+import Comic1 from "./../img/Comic1.jpg";
+// import PropTypes from "prop-types";
 
-function Comic(){
+function Comic(props){
   return (
     <React.Fragment>
       <img src={Comic1} alt="Fake Comic" />
-      <h3>This is a comic</h3>
-      <h4>Price: $10</h4>
+      <h3>{props.title}</h3>
+      <h4>Condition: {props.condition}</h4>
+      <h4>Date: {props.date}</h4>
+      <h4>Price: {props.price}</h4>
     </React.Fragment>
   );
 }
+
+// Comic.propTypes = {
+//   title: PropTypes.string,
+//   condition: PropTypes.string,
+//   date: PropTypes.string,
+//   price: PropTypes.string,
+// }
 
 export default Comic;
--- src/components/ComicList.js
+++ src/components/ComicList.js
@@ -1,9 +1,22 @@
 import React from "react";
 import Comic from "./Comic";
 
+
 function ComicList() {
   return (
-    <Comic />
+    <React.Fragment>
+    <Comic 
+    // <img src={Comic1} alt="Fake Comic" />
+    title="this is a comic"
+    condition="F-"
+    date="some date"
+    price="$10"/>
+    <Comic
+    title="this is a comic also"
+    condition="F"
+    date="some other date"
+    price="$40"/>
+    </React.Fragment>
   );
 }
 
