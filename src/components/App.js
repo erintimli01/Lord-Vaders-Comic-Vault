55ac5e042d046a5197982f460df80df4e7d31f52
Erin Timlin <erintimlin@gmail.com>
2023-05-14 18:52:28 -0400

    move mainComicList into app, update routes

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,42 @@ import ComicList from "./ComicList";
 import ComicDetails from "./ComicDetails";
 import { Route, Routes } from 'react-router-dom';
 
+import Comic1 from "./../img/Comic1.jpg";
+import Comic2 from "./../img/Comic2.jpg";
+
+const mainComicList = [
+  {
+    id: 1,
+    title: "this is a comic",
+    condition: "F-",
+    date: "some date",
+    price: "$10",
+    image: Comic1
+  },
+  {
+    id: 2,
+    title: "this is a comic also",
+    condition: "F",
+    date: "some other date",
+    price: "$40",
+    image: Comic2
+  }
+];
 
 function App() {
   return (
     <>
     <Header />
     <Routes>
-      <Route path="/" element={<ComicList />} />
-      <Route path="/comic/:id" element={<ComicDetails />} />
+    <Route path="/" element={<ComicList comics={mainComicList} />} />
+      {/* <Route path="/" element={<ComicList />} /> */}
+      {/* <Route path="/comic/:id" element={<ComicDetails />} /> */}
+      <Route
+        path="/comic/:id"
+        element={<ComicDetails comics={mainComicList}
+    />
+  }
+/>
     </Routes>
     </>
   )
