38484886ff35920cac18ca5e18afc319a6cb3f7d
Erin Timlin <erintimlin@gmail.com>
2023-05-14 15:27:16 -0400

    import routes and route paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,31 @@
 import React from "react";
 import Header from "./Header";
 import ComicList from "./ComicList";
+import ComicDetails from "./ComicDetails";
+import { Route, Routes } from 'react-router-dom';
 
 
-function App(){
+function App() {
   return (
-    <React.Fragment>
-      <Header />
-      <ComicList />
-
-      <hr/>
-    </React.Fragment>
-  );
+    <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<ComicList />} />
+      <Route path="/comic/:id" element={<ComicDetails />} />
+    </Routes>
+    </>
+  )
 }
 
-export default App;
+// function App(){
+//   return (
+//     <React.Fragment>
+//       <Header />
+//       <ComicList />
+
+//       <hr/>
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
