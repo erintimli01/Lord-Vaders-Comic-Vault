762ca6e0696ccc950fe6ce1c8a3e3deeda504005
Erin Timlin <erintimlin@gmail.com>
2023-05-16 12:52:10 -0400

    add basic, broken navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './NavBar';
 import Header from "./Header";
 import ComicList from "./ComicList";
 import ComicDetails from "./ComicDetails";
-import { Route, Routes } from 'react-router-dom';
 
 import Comic1 from "./../img/Comic1.jpg";
 import Comic2 from "./../img/Comic2.jpg";
@@ -29,23 +30,69 @@ const mainComicList = [
 function App() {
   return (
     <>
-    <Header />
-    <Routes>
-    <Route path="/" element={<ComicList comics={mainComicList} />} />
-      {/* <Route path="/" element={<ComicList />} /> */}
-      {/* <Route path="/comic/:id" element={<ComicDetails />} /> */}
-      <Route
-        path="/comic/:id"
-        element={<ComicDetails comics={mainComicList}
-    />
-  }
-/>
-    </Routes>
+      <NavBar />
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<ComicList comics={mainComicList} />} />
+        <Route path="/comic/:id" element={<ComicDetails comics={mainComicList} />} />
+      </Routes>
     </>
   )
 }
 
 export default App;
+
+// import React from "react";
+// import Header from "./Header";
+// import ComicList from "./ComicList";
+// import ComicDetails from "./ComicDetails";
+// import ReactDOM from "react-dom";
+// import NavBar from './NavBar';
+// import { Route, Routes } from 'react-router-dom';
+
+// import Comic1 from "./../img/Comic1.jpg";
+// import Comic2 from "./../img/Comic2.jpg";
+
+// const mainComicList = [
+//   {
+//     id: 1,
+//     title: "this is a comic",
+//     condition: "F-",
+//     date: "some date",
+//     price: "$10",
+//     image: Comic1
+//   },
+//   {
+//     id: 2,
+//     title: "this is a comic also",
+//     condition: "F",
+//     date: "some other date",
+//     price: "$40",
+//     image: Comic2
+//   }
+// ];
+
+// function App() {
+//   return (
+//     <>
+//     <NavBar />
+//     <Header />
+//     <Routes>
+//     <Route path="/" element={<ComicList comics={mainComicList} />} />
+//       {/* <Route path="/" element={<ComicList />} /> */}
+//       {/* <Route path="/comic/:id" element={<ComicDetails />} /> */}
+//       <Route
+//         path="/comic/:id"
+//         element={<ComicDetails comics={mainComicList}
+//     />
+//   }
+// />
+//     </Routes>
+//     </>
+//   )
+// }
+
+// export default App;
 
 // function App(){
 //   return (
