6120d2561196c2490136d4d6f6614087e4078ba6
Erin Timlin <erintimlin@gmail.com>
2023-05-17 12:02:32 -0400

    remove commented out code

--- src/components/ComicDetails.js
+++ src/components/ComicDetails.js
@@ -45,23 +45,3 @@ ComicDetails.propTypes = {
 };
 
 export default ComicDetails;
-
-// function ComicDetails(props) {
-//   return (
-//   <div>
-//     <h2>{props.title}</h2>
-//     <p>Condition: {props.condition} </p>
-//     <p>Date: {props.date}</p>
-//     <p>Price: {props.price}</p>
-//   </div>
-//   );
-// }
-
-// ComicDetails.propTypes = {
-//   title: PropTypes.string,
-//   condition: PropTypes.string,
-//   date: PropTypes.string,
-//   price: PropTypes.string,
-// };
-
-// export default ComicDetails;
