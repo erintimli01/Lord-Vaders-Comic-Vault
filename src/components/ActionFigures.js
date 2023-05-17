e0e82e4753b45d9b6036cadcd5c5fbe0d2df3a8e
Erin Timlin <erintimlin@gmail.com>
2023-05-17 12:15:42 -0400

    add action figures page

---
+++ src/components/ActionFigures.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ActionFigures() {
+  return (
+    <div>
+      <h2>Action Figures</h2>
+      <p>Coming Soon!</p>
+    </div>
+  );
+}
+
+export default ActionFigures;
