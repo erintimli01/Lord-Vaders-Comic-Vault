cd216eb0c495c2a952ee911fec5384f262bf26fd
Erin Timlin <erintimlin@gmail.com>
2023-05-17 12:52:27 -0400

    add movie posters component

---
+++ src/components/MoviePosters.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MoviePosters() {
+  return (
+    <div>
+      <h2>Movie Posters</h2>
+      <p>Coming Soon!</p>
+    </div>
+  );
+}
+
+export default MoviePosters;
