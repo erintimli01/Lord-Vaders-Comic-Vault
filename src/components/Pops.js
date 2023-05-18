12e5015ee99ea4b06aae16d0b3b2823227285e2c
Erin Timlin <erintimlin@gmail.com>
2023-05-18 13:20:39 -0400

    update styling

--- src/components/Pops.js
+++ src/components/Pops.js
@@ -1,12 +1,69 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const containerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "30%",
+  margin: "0 auto",
+  padding: "20px",
+  border: "1px solid #ccc",
+  borderRadius: "5px",
+  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+  backgroundColor: "white",
+};
+
+const imageStyle = {
+  width: "200px",
+  height: "300px",
+};
+
+const buttonStyle = {
+  marginTop: "20px",
+  padding: "8px 16px",
+  backgroundColor: "#007bff",
+  color: "#fff",
+  border: "none",
+  borderRadius: "4px",
+  cursor: "pointer",
+};
+
+const ComingSoonContainer = styled.div`
+  ${containerStyle}
+`;
 
 function Pops() {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate("/");
+  };
+
   return (
-    <div>
+    <ComingSoonContainer>
       <h2>Pops</h2>
       <p>Coming Soon!</p>
-    </div>
+      <button style={buttonStyle} onClick={handleBack}>
+        Back
+      </button>
+    </ComingSoonContainer>
   );
 }
+
+Pops.propTypes = {
+  comics: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      title: PropTypes.string,
+      description: PropTypes.string,
+      condition: PropTypes.string,
+      date: PropTypes.string,
+      price: PropTypes.string,
+    })
+  ),
+};
 
 export default Pops;
